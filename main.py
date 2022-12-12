7a30da62a50698863cf5f5f490ba6d92e43731a4
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-12-12 20:00:33 +0800

    main: Code cleanup and TODOs
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
-
-import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
-
 from api_token import TOKEN
 
-async def start(update, context):
-    await update.message.reply_text("Hello, World!")
+# TODO: Implement these modules (ref: github.com/Hakimi0804/tgbot-fish)
+# import modules.spam_protect     # Prevent spammers in groups
+# import modules.rm6785           # RM6785 community management
+# import modules.moderation       # /ban, /kick, etc
+# import modules.komaru           # Pranaya's komaru GIFs channel management
+# import modules.toys             # Useless stuffs for fun, e.g. /shuf, etc
+# import modules.help             # /help command (perhaps we can make it util.help instead)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("A bot written in Python3, by @Hakimi0804.")
 
 
 app = ApplicationBuilder().token(TOKEN).build()
