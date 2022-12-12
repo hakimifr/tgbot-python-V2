439641688df55ae99ec28a00ff22eff260e2ad5f
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-12-12 17:11:05 +0800

    Create config module
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

---
+++ util/config.py
@@ -0,0 +1,48 @@
+import json
+import atexit
+import logging
+from pathlib import Path
+log = logging.getLogger("config")
+
+
+class Config:
+    def __init__(self, file: str):
+        self.write_pending: bool = False
+        self._config: dict = {}
+        self.file: str = file
+
+        # Automatically load config from file if exist
+        if Path(self.file).exists() and Path(self.file).is_file():
+            log.info(f"Auto-loading config from {self.file} since it exists")
+            self.read_config()
+
+        # Make sure changes are written upon exit
+        atexit.register(self.on_exit)
+
+    def on_exit(self) -> None:
+        if not self.write_pending:
+            log.info("No need to save changes")
+
+        log.info("Writing unsaved changes")
+        self.write_config()
+
+    def write_config(self) -> None:
+        log.info(f"Writing config to {self.file}")
+        with open(self.file, "w") as config_file:
+            json.dump(self.config, config_file)
+        self.write_pending = False
+
+    def read_config(self) -> None:
+        log.info(f"Reading config from {self.file}")
+        with open(self.file, "r") as config_file:
+            self.config = json.load(config_file)
+
+    @property
+    def config(self) -> dict:
+        return self._config
+
+    @config.setter
+    def config(self, value) -> None:
+        self._config = value
+        self.write_pending = True
+
