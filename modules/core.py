af127086d8aedce073cb1684b95cc4e85065527a
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2023-01-30 11:29:45 +0800

    modules.core: Do not save if not sent by owner

--- modules/core.py
+++ modules/core.py
@@ -1,17 +1,22 @@
 import sys
+import logging
 import subprocess
 from util.help import Help
 from telegram import Update
 from telegram.ext import ContextTypes
 
 SAVED_MESSAGE_CHAT_ID = -1001607510711
-
+log = logging.getLogger("modules.core")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("A bot written in Python3, by @Hakimi0804.")
 
 
 async def save(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.from_user.id != 1024853832:
+        log.debug("/save was not sent by owner, ignoring")
+        return
+
     if update.message.reply_to_message is None:
         await update.message.reply_text("You must reply to as message.")
     else:
