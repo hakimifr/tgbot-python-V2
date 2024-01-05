e5788873e8161fc5f7805aeb96ba48bdfb044abb
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2024-01-06 01:42:14 +0800

    credentials.py: Take other stuff into consideration

--- credentials.py
+++ credentials.py
@@ -4,10 +4,18 @@ from getpass import getpass
 
 print("Note: only need to be ran once. Output is intentionally disabled.")
 
-token = getpass("Paste token here: ")
-with open("api_token.py", "w") as file:
-    file.write("TOKEN = \"" + token + "\"\n")
+secrets = [
+    "TOKEN",
+    "GITHUB_TOKEN",
+    "OPENAI_API_KEY"
+]
 
+with open("api_token.py", "a", encoding="utf-8") as file:
+    for secret in secrets:
+        token = getpass(f"Enter {secret} here: ")
+        file.write(f'{secret} = "{token}"\n')
 
-print(f"Saved token with length: {len(token)}")
+        print(f" -> Saved {secret} with length: {len(token)}")
+
+print(f"\nSaved api_token.py with all credentials.")
 
